2bb07d0a3d4967b2450dc04fc5d7fad390ab0598
volodya <volodyapabyr@gmail.com>
2016-12-07 16:40:12 +0200

    2

--- Test Winforms Proj/Test Winforms Proj/Form1.cs
+++ Test Winforms Proj/Test Winforms Proj/Form1.cs
@@ -19,6 +19,7 @@ namespace Test_Winforms_Proj
             Load += Form1_Load;
             Closing += Form1_Closing;
             Closing += Form1_Closing;
+            Closing += Form1_Closing;
         }
 
         private void Form1_Closing(object sender, CancelEventArgs e)
